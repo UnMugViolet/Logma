ee939a04bef6310c1ff322a931a4cba8eb0af1d9
UnMugViolet <jaguinpaul@gmail.com>
2023-07-11 17:36:18 +0200

    Create all pages for website

---
+++ contacts.php
@@ -0,0 +1,25 @@
+<?php
+ $name = "Paul "; 
+ echo "<h1>Bonjour ! ce site a été conçu par</h1> {$name}"; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mentions Légales | Logma </title>
+
+    <!--Feuille de CSS-->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- JS -->
+    <script src="index.js" type="text/javascript" defer></script>
+    <script src="./js/components/header.js" type="text/javascript" defer></script>
+    <!-- Favicon -->
+    <link rel="icon" href="./ressources/favicon.ico">
+
+</head>
+<body>
+    <header-components></header-components>
+</body>
+</html>
