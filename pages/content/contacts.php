cd6671d9e992420635bac2cb453ae36b43c1d758
UnMugViolet <jaguinpaul@gmail.com>
2023-07-26 21:07:17 +0200

    Translate form

--- pages/content/contacts.php
+++ pages/content/contacts.php
@@ -36,46 +36,49 @@
         $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
         $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
         if(!preg_match("/^[A-Za-z .'-]+$/", $name)){
-          $name_error = 'Invalid name';
+          $name_error = 'Votre Nom n\'est pas valide';
         }
         if(!preg_match("/^[A-Za-z .'-]+$/", $subject)){
-          $subject_error = 'Invalid subject';
+          $subject_error = 'Votre sujet n\'est pas valide';
         }
         if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)){
-          $email_error = 'Invalid email';
+          $email_error = 'Votre e-mail n\'est pas valide';
         }
         if(strlen($message) === 0){
-          $message_error = 'Your message should not be empty';
+          $message_error = 'Votre message ne doit pas être vide';
         }
       }
     ?>
     
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-      <label for="name">Name:</label><br>
-      <input type="text" name="name">
-      <p><?php if(isset($name_error)) echo $name_error; ?></p>
-      <label for="subject">Subject:</label><br>
-      <input type="text" name="subject">
-      <p><?php if(isset($subject_error)) echo $subject_error; ?></p>
-      <label for="email">Email:</label><br>
-      <input type="text" name="email">
-      <p><?php if(isset($email_error)) echo $email_error; ?></p>
-      <label for="message">Message:</label><br>
-      <textarea name="message"></textarea>
-      <p><?php if(isset($message_error)) echo $message_error; ?></p>
-      <input type="submit" name="submit" value="Submit">
-      <?php 
-        if(isset($_POST['submit']) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)){
-          $to = 'youremail@addres.com'; // edit here
-          $body = " Name: $name\n E-mail: $email\n Message:\n $message";
-          if(mail($to, $subject, $body)){
-            echo '<p style="color: green">Message sent</p>';
-          }else{
-            echo '<p>Error occurred, please try again later</p>';
+
+    <div class="spacing-section">
+      <form class="spacing-section" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+        <label for="name"></label><br>
+        <input type="text" name="name" placeholder="Nom">
+        <p><?php if(isset($name_error)) echo $name_error; ?></p>
+        <label for="subject"></label><br>
+        <input type="text" placeholder="Objet">
+        <p><?php if(isset($subject_error)) echo $subject_error; ?></p>
+        <label for="email"></label><br>
+        <input type="text" name="email" placeholder="E-mail">
+        <p><?php if(isset($email_error)) echo $email_error; ?></p>
+        <label for="message"></label><br>
+        <textarea name="message" placeholder="Votre Message"></textarea>
+        <p><?php if(isset($message_error)) echo $message_error; ?></p>
+        <input type="submit" name="submit" value="Envoyer">
+        <?php 
+          if(isset($_POST['submit']) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)){
+            $to = 'contact@logma-production.com'; // edit here
+            $body = " Name: $name\n E-mail: $email\n Message:\n $message";
+            if(mail($to, $subject, $body)){
+              echo '<p style="color: green">Message envoyé avec succès</p>';
+            }else{
+              echo '<p>Error occurred, please try again later</p>';
+            }
           }
-        }
-      ?>
-    </form>
+        ?>
+      </form>
+    </div>
     <footer-component class="absolute bottom-0"/>
 </body>
 
