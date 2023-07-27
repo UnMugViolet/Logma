69d580520103cabc3c6a61e56149605536d3502a
UnMugViolet <jaguinpaul@gmail.com>
2023-07-27 14:08:32 +0200

    Added favicon everywhere + refacto cgu & mentions légales

--- pages/content/contacts.php
+++ pages/content/contacts.php
@@ -6,8 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Contacts | Logma </title>
+    <title>Contact | Logma </title>
 
     <!--Feuille de CSS-->
     <link rel="stylesheet" href="css/main.css">
@@ -18,11 +17,11 @@
     <script src="./js/script.js" type="text/javascript" defer></script>
     
     <!-- Favicon -->
-    <link rel="icon" href="/ressources/favicon.ico">
+    <link rel="icon" href="favicon.ico">
 
 </head>
 
-<body>
+<body >
     <section>
         <div>
             <header-component/>
