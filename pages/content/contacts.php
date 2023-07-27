a766e3c5a565ac5db158571c96b2de9cc0f24286
UnMugViolet <jaguinpaul@gmail.com>
2023-07-27 12:18:22 +0200

    Fix issue with redirecting on self

--- pages/content/contacts.php
+++ pages/content/contacts.php
@@ -52,31 +52,31 @@
     
 
     <div class="spacing-section">
-      <form class="spacing-section" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-        <label for="name"></label><br>
-        <input type="text" name="name" placeholder="Nom">
-        <p><?php if(isset($name_error)) echo $name_error; ?></p>
-        <label for="subject"></label><br>
-        <input type="text" placeholder="Objet">
-        <p><?php if(isset($subject_error)) echo $subject_error; ?></p>
-        <label for="email"></label><br>
-        <input type="text" name="email" placeholder="E-mail">
-        <p><?php if(isset($email_error)) echo $email_error; ?></p>
-        <label for="message"></label><br>
-        <textarea name="message" placeholder="Votre Message"></textarea>
-        <p><?php if(isset($message_error)) echo $message_error; ?></p>
-        <input type="submit" name="submit" value="Envoyer">
-        <?php 
-          if(isset($_POST['submit']) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)){
-            $to = 'contact@logma-production.com'; // edit here
-            $body = " Name: $name\n E-mail: $email\n Message:\n $message";
-            if(mail($to, $subject, $body)){
-              echo '<p style="color: green">Message envoyé avec succès</p>';
-            }else{
-              echo '<p>Error occurred, please try again later</p>';
+        <form action="" method="POST">
+            <label for="name"></label><br>
+            <input type="text" name="name" placeholder="Nom">
+            <p><?php if(isset($name_error)) echo $name_error; ?></p>
+            <label for="subject"></label><br>
+            <input type="text" placeholder="Objet">
+            <p><?php if(isset($subject_error)) echo $subject_error; ?></p>
+            <label for="email"></label><br>
+            <input type="text" name="email" placeholder="E-mail">
+            <p><?php if(isset($email_error)) echo $email_error; ?></p>
+            <label for="message"></label><br>
+            <textarea name="message" placeholder="Votre Message"></textarea>
+            <p><?php if(isset($message_error)) echo $message_error; ?></p>
+            <input type="submit" name="submit" value="Envoyer">
+            <?php 
+            if(isset($_POST['submit']) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)){
+                $to = 'contact@logma-production.com'; // edit here
+                $body = " Name: $name\n E-mail: $email\n Message:\n $message";
+                if(mail($to, $subject, $body)){
+                echo '<p style="color: green">Message envoyé avec succès</p>';
+                }else{
+                echo '<p>Error occurred, please try again later</p>';
+                }
             }
-          }
-        ?>
+            ?>
       </form>
     </div>
     <footer-component class="absolute bottom-0"/>
