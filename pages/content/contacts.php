4b6b9a623ec2f57677f434def1b260edc90988d2
UnMugViolet <jaguinpaul@gmail.com>
2023-07-27 09:51:27 +0200

    🤡 remove crap

--- pages/content/contacts.php
+++ pages/content/contacts.php
@@ -9,33 +9,11 @@ echo "<h1>Contact réalisée par {$name}</h1> ";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-<<<<<<<< HEAD:pages/content/contacts.php
+
     <title>Contacts | Logma </title>
 
     <!--Feuille de CSS-->
     <link rel="stylesheet" href="css/main.css">
-<<<<<<< HEAD
-========
-    <title>CGU | Logma </title>
-=======
-<<<<<<<< HEAD:pages/legal/mentions-legales.html
-    <title>Mentions légales | Logma </title>
-========
-    <title>Contacts | Logma </title>
->>>>>>>> c91937f (🔍 Added all pages and make them work with footer and header, great day today big progress):pages/content/contacts.php
->>>>>>> c91937f (🔍 Added all pages and make them work with footer and header, great day today big progress)
-
-    <!--Feuille de CSS-->
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/specific/header.css">
-    <link rel="stylesheet" href="css/specific/footer.css">
-<<<<<<< HEAD
->>>>>>>> c91937f (🔍 Added all pages and make them work with footer and header, great day today big progress):pages/legal/cgu.html
-=======
->>>>>>> c91937f (🔍 Added all pages and make them work with footer and header, great day today big progress)
-=======
->>>>>>> e6b9ae5 (All done can close this issue :fire:)
 
     <!-- JS -->
     <script src="./js/components/header.js" type="text/javascript"></script>
@@ -52,14 +30,6 @@ echo "<h1>Contact réalisée par {$name}</h1> ";
         <div>
             <header-component/>
         </div>
-<<<<<<< HEAD
-        <h1>CGU</h1>
-=======
-<<<<<<<< HEAD:pages/legal/mentions-legales.html
-        <h1>Mentions légales</h1>
-========
->>>>>>>> c91937f (🔍 Added all pages and make them work with footer and header, great day today big progress):pages/content/contacts.php
->>>>>>> c91937f (🔍 Added all pages and make them work with footer and header, great day today big progress)
     </section>
     
     <footer-component/>
