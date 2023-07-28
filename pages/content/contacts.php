1268463f29cb93449f143b7ce6595e6fcdad37bf
UnMugViolet <jaguinpaul@gmail.com>
2023-07-28 11:58:27 +0200

    ✅ Added responsive to contact page + created it finished now

--- pages/content/contacts.php
+++ pages/content/contacts.php
@@ -39,22 +39,22 @@
               $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
               $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
               if(!preg_match("/^[A-Za-z .'-]+$/", $name)){
-                $name_error = 'Votre Nom n\'est pas valide';
+                $name_error = '<p class="color-white">Votre Nom n\'est pas valide</p>';
               }
               if(!preg_match("/^[A-Za-z .'-]+$/", $subject)){
-                $subject_error = 'Votre sujet n\'est pas valide';
+                $subject_error = '<p class="color-white">Votre sujet n\'est pas valide</p>';
               }
               if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)){
-                $email_error = 'Votre e-mail n\'est pas valide';
+                $email_error = '<p class="color-white">Votre e-mail n\'est pas valide</p>';
               }
               if(strlen($message) === 0){
-                $message_error = 'Votre message ne doit pas être vide';
+                $message_error = '<p class="color-white">Votre message n\'est pas rempli</p>';
               }
             }
           ?>
           
 
-          <div class="w*-full">
+          <div class="w-full">
             <h1 class="color-white">On reste en contact ?</h1>
 
             <form action="" method="POST">
@@ -82,7 +82,7 @@
                       if(mail($to, $subject, $body)){
                       echo '<p style="color: green">Message envoyé avec succès</p>';
                       }else{
-                      echo '<p>Une erreur s\'est produite, vous pouvez nous contacter à contact@logma-production.com</p>';
+                      echo '<p class="color-white">Une erreur s\'est produite, vous pouvez nous contacter à contact@logma-production.com</p>';
                       }
                   }
                   ?>
@@ -90,13 +90,35 @@
           </div>
         </div>
 
-        <div class="container col-2">
-          <p class="color-white text-center">fehbzuihfiafd</p>
+        <div class="container col-2 vertical-align spacing-section w-full">
+          <div>
+            <div>
+              <div class="object-center margin-whatsapp">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+                  <path fill="#ffffff" d="M19.05 4.91A9.816 9.816 0 0 0 12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01zm-7.01 15.24c-1.48 0-2.93-.4-4.2-1.15l-.3-.18l-3.12.82l.83-3.04l-.2-.31a8.264 8.264 0 0 1-1.26-4.38c0-4.54 3.7-8.24 8.24-8.24c2.2 0 4.27.86 5.82 2.42a8.183 8.183 0 0 1 2.41 5.83c.02 4.54-3.68 8.23-8.22 8.23zm4.52-6.16c-.25-.12-1.47-.72-1.69-.81c-.23-.08-.39-.12-.56.12c-.17.25-.64.81-.78.97c-.14.17-.29.19-.54.06c-.25-.12-1.05-.39-1.99-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.14-.25-.02-.38.11-.51c.11-.11.25-.29.37-.43s.17-.25.25-.41c.08-.17.04-.31-.02-.43s-.56-1.34-.76-1.84c-.2-.48-.41-.42-.56-.43h-.48c-.17 0-.43.06-.66.31c-.22.25-.86.85-.86 2.07c0 1.22.89 2.4 1.01 2.56c.12.17 1.75 2.67 4.23 3.74c.59.26 1.05.41 1.41.52c.59.19 1.13.16 1.56.1c.48-.07 1.47-.6 1.67-1.18c.21-.58.21-1.07.14-1.18s-.22-.16-.47-.28z"/>
+                </svg>
+              </div>
+
+              <div class="object-center margin-number">
+                <a class="object-center" href="tel:+0679796970">06 79 79 69 70</a>
+              </div>
+
+            </div>
+            <div>
+              <img src="./ressources/img/baptiste-fablet.jpg" alt="">
+            </div>
+          </div>
+
         </div>
+
       </div>
     </section>
-    <div class="container bg-color-black">
-      <hr class="small-line container" />
+
+
+    <div class="bg-color-black">
+      <div class="bg-color-black">
+        <hr class="container small-line "  />
+      </div>
     </div>
     
     <footer-component/>
