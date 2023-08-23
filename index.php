64d82baa1d5bf17e2636c7da6d01b2245b29e496
UnMugViolet <jaguinpaul@gmail.com>
2023-08-23 22:29:57 +0200

    Remove useless call to header and footer

--- index.php
+++ index.php
@@ -10,10 +10,7 @@
     <link rel="stylesheet" href="css/main.css">
 
     <!-- JS -->
-    <script src="./js/components/header.js" type="text/javascript"></script>
-    <script src="./js/components/footer.js" type="text/javascript"></script>
-
-    <script src="./js/components/typewirter.js" type="text/javascript"></script>
+    <script src="./js/components/typewirter.js" type="text/javascript" defer></script>
     
     <script src="./js/script.js" type="text/javascript" defer></script>
     
