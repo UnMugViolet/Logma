a79c2e32b8b34c9fc2476893b1f346f0ac3251ec
UnMugViolet <jaguinpaul@gmail.com>
2023-09-05 18:22:50 +0200

    Add logs for logging in and out

--- classes/login.classes.php
+++ classes/login.classes.php
@@ -8,33 +8,59 @@ class Login extends Dbh {
 
         if (!$stmt->execute(array($uid, $uid))){
             $stmt = null;
+            $this->logEvent("SQL statement execution failed", "ERROR");
             header("location: ../access-admin-logma?error=stmtfailed");
             exit();
         }
         
 
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+        
+        
         if (!$user){
             $stmt = null;
-            header("location: ../access-admin-logma?error=usernotfound");
+            $this->logEvent("Connexion attempt with unknow user", "ERROR");
+            header("location: ../access-admin-logma?error=wronginformations");
             exit();
         }
         
         $checkPwd = password_verify($pwd, $user["users_pwd"]);
 
         if (!$checkPwd){
+            $this->logEvent("Connexion attempt with known user", "WARNING");
             header("location: ../access-admin-logma?error=wronginformations");
             exit();
         }
+
+
+
         else {
             session_start();
             $_SESSION["userid"] = $user["users_id"];
             $_SESSION["useruid"] = $user["users_uid"];
             $_SESSION["userrole"] = $user["users_role"];
             $_SESSION['last_activity'] = $_SERVER['REQUEST_TIME'];
+
+            $this->logEvent("Successful login for user: " . $user["users_uid"]);
             $stmt = null;
         }
+    }
+
+    private function logEvent($message, $severity = "INFO") {
+        $logFile = "../log/login_log.txt";
+        $timestamp = date("Y-m-d H:i:s");
+
+        $severityIcons = [
+            "INFO" => "ℹ️",
+            "WARNING" => "⚠️",
+            "ERROR" => "❌",
+        ];
+
+        $severityIcon = isset($severityIcons[$severity]) ? $severityIcons[$severity] : $severityIcons["INFO"];
+        $logMessage = "[$timestamp] $severityIcon $message" . PHP_EOL;
+
+        // Append the log message to the log file
+        file_put_contents($logFile, $logMessage, FILE_APPEND);
     }
 }
 ?>
--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,10 +1,35 @@
 <?php
 
 session_start();
+
+// Log a successful logout
+logEvent("SUCCESS: User logged out successfully", "OK");
+
+// Unset and destroy the session
 session_unset();
 session_destroy();
 
-
 // Going back to root
 header("location: ../index.php?error=none");
+
+// Function to log events with severity indicators
+function logEvent($message, $severity = "INFO") {
+    $logFile = "../log/login_log.txt";
+    $timestamp = date("Y-m-d H:i:s");
+
+    // Define ASCII characters for severity indicators
+    $severityIcons = [
+        "OK" => "✅",
+        "WARNING" => "⚠️",
+        "ERROR" => "❌",
+    ];
+
+    // Check if the specified severity exists in the array, otherwise default to "INFO"
+    $severityIcon = isset($severityIcons[$severity]) ? $severityIcons[$severity] : $severityIcons["INFO"];
+
+    $logMessage = "[$timestamp] $severityIcon $message" . PHP_EOL;
+
+    // Append the log message to the log file
+    file_put_contents($logFile, $logMessage, FILE_APPEND);
+}
 
