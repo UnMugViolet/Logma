67c464556b80c87010e22f04596e320fab3e4c52
UnMugViolet <jaguinpaul@gmail.com>
2023-09-11 09:04:23 +0200

    📱 Improve responsive Layout

--- pages/admin/gallery.php
+++ pages/admin/gallery.php
@@ -29,7 +29,7 @@
 </head>
 
 <body class="bg-color-black">
-    <section class="h-full-screen">
+    <section class="h-full-screen-responsive">
         <div class="container h-full vertical-align object-center">
             <div>
                 <div class="block container icon-error mt-50 mb-50">
